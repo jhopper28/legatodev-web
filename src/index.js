5987c63404d335743c8afdc4d157a41245c819fb
Jeff Hopper <Jeff.Hopper@LevelUpDevelopment.com>
2017-03-28 11:25:52 -0400

    Started fleshing out app structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+render(
+  <App/>,
   document.getElementById('root')
 );
